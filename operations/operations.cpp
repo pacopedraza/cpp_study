5febe37353b473c77019fec5897c7a1486f26ce5
Francisco Pedraza <pacopedraza87@gmail.com>
2018-08-31 21:02:03 -0500

    Adds basic hello world and basic operations program
    This is just to start warm up on C++ language.
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

---
+++ operations/operations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    // Declaring variables
+    int a, b;
+    int result;
+
+    // Process
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+
+    // Print out the result
+    std::cout << result;
+
+    // terminate program
+    return 0;
+}
